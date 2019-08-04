98d98878de9f697cbadeb2acf3c45dbb52fccaeb
Maaruf Dauda <mkdauda.work@gmail.com>
2019-08-04 10:07:16 +0100

    rename activity accordingly

--- app/src/main/java/com/emkaydauda/travelmantics/InsertActivity.kt
+++ app/src/main/java/com/emkaydauda/travelmantics/InsertActivity.kt
@@ -3,10 +3,10 @@ package com.emkaydauda.travelmantics
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class InsertActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_insert)
     }
 }
