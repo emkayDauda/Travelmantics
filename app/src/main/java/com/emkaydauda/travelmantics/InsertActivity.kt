c05d84fad6f3b66648d2c2c216eab106dbb236f3
Maaruf Dauda <mkdauda.work@gmail.com>
2019-08-04 12:28:44 +0100

    Complete logic for database send

--- app/src/main/java/com/emkaydauda/travelmantics/InsertActivity.kt
+++ app/src/main/java/com/emkaydauda/travelmantics/InsertActivity.kt
@@ -2,11 +2,39 @@ package com.emkaydauda.travelmantics
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.emkaydauda.travelmantics.models.TravelDeals
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_insert.*
 
 class InsertActivity : AppCompatActivity() {
 
+    lateinit var firebaseDatabase: FirebaseDatabase
+    lateinit var firebaseDatabaseReference: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_insert)
+
+        firebaseDatabase = FirebaseDatabase.getInstance()
+        firebaseDatabaseReference  = firebaseDatabase.reference.child("travelDeals")
+
+        saveDeal.setOnClickListener {
+            saveDeal(TravelDeals(inputName.text.toString(), inputPrice.text.toString(), inputDescription.text.toString(),""))
+
+        }
+    }
+
+    private fun saveDeal(deal: TravelDeals){
+
+        firebaseDatabaseReference.push().setValue(deal)
+
+        clean()
+    }
+
+    private fun clean(){
+        inputName.setText("")
+        inputPrice.setText("")
+        inputDescription.setText("")
+        inputName.requestFocus()
     }
 }
---
+++ app/src/main/java/com/emkaydauda/travelmantics/models/TravelDeals.kt
@@ -0,0 +1,3 @@
+package com.emkaydauda.travelmantics.models
+
+data class TravelDeals(val title: String, val description: String, val price: String, val imageUrl: String)
